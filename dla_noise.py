19ac29b60e3393245996bfe3b35ae7b2399b5ed0
Arjun Sheshadri <64653918+GELIOS-19@users.noreply.github.com>
2024-06-27 01:00:12 -0400

    g

--- dla_noise.py
+++ dla_noise.py
@@ -103,6 +103,7 @@ class Pixel:
         frozen: bool
         weight: float
         struck: Optional[Self]
+        coords: tuple[int, int]
 
         def __init__(self, x: int, y: int):
                 self.x = x
@@ -112,6 +113,10 @@ class Pixel:
                 self.frozen = False
                 self.weight = 0
                 self.struck = None
+
+        @property
+        def coords(self) -> tuple[int, int]:
+                return self.x, self.y
 
 
 class Image:
@@ -393,18 +398,12 @@ def simulate_random_walk(image: Image, num_concurrent_walkers: int):
         #       we can use bresenham's line algorithm to roughly model an equation that
         #       follows the edges of this geometry. This edges tuple can be generated
         #       dependent on the geometry
-        edges = (
-                        ((0, image.size - 1), (0, 0)),  # Top
-                        ((0, image.size - 1), (image.size - 1, image.size - 1)),  # Bottom
-                        ((image.size - 1, image.size - 1), (0, image.size - 1)),  # Right
-                        ((0, 0), (0, image.size - 1)),  # Left
-        )
+        edges = image.boundary.edges
 
         walkers = []
         for walker in range(num_concurrent_walkers):
                 edge = random.choice(edges)
-                x = random.randint(edge[0][0], edge[0][1])
-                y = random.randint(edge[1][0], edge[1][1])
+                x, y = random.choice(edge)
                 while image.grid[x][y].frozen:
                         x = random.randint(0, image.size - 1)
                         y = random.randint(0, image.size - 1)
@@ -416,27 +415,25 @@ def simulate_random_walk(image: Image, num_concurrent_walkers: int):
         while walkers:
                 for i, path in enumerate(walkers):
                         direction = random.choice(GLOBAL_DIRECTIONS)
-                        x, _ = constrain(path[-1][0] + direction[0], 0, image.size - 1)
-                        y, _ = constrain(path[-1][1] + direction[1], 0, image.size - 1)
 
-                        if image.grid[x][y].frozen:
+                        if image[path[-1][0] + direction[0], path[-1][1] + direction[1]].frozen:
                                 previous_x = path[-1][0]
                                 previous_y = path[-1][1]
 
                                 pixel = image.grid[previous_x][previous_y]
-                                pixel.struck = image.grid[x][y]
+                                pixel.struck = image[path[-1][0] + direction[0], path[-1][1] + direction[1]]
                                 pixel.frozen = True
                                 pixel.weight = 100
                                 walkers.pop(i)
 
-                        path.append((x, y))
+                        path.append(image[path[-1][0] + direction[0], path[-1][1] + direction[1]].coords)
 
         return walkers
 
 
 def crisp_upscale(traversable_image: Image, new_image_size: int) -> Image:
-        new_image = Image(new_image_size)
         scale_factor = new_image_size / traversable_image.size
+        new_image = Image(Boundary([[int(point[0] * scale_factor), int(point[1] * scale_factor)] for point in traversable_image.boundary.points]))
 
         new_image.origin = new_image.grid[int(traversable_image.origin.x * scale_factor)][int(traversable_image.origin.y * scale_factor)]
 
@@ -483,7 +480,7 @@ def jitter_contiguous_lines(traversable_image: Image) -> Image:
 
 
 def apply_downstream_height(traversable_image: Image, clamp: int) -> Image:
-        new_image = Image(traversable_image.size)
+        new_image = Image(traversable_image.boundary)
 
         new_image.origin = new_image.grid[traversable_image.origin.x][traversable_image.origin.y]
 
@@ -540,8 +537,8 @@ def apply_downstream_height(traversable_image: Image, clamp: int) -> Image:
 
 
 def bilinear_upscale(image: Image, new_image_size: int) -> Image:
-        new_image = Image(new_image_size)
         scale_factor = new_image_size / image.size
+        new_image = Image(Boundary([[int(point[0] * scale_factor), int(point[1] * scale_factor)] for point in image.boundary.points]))
 
         for i in range(new_image_size):
                 for j in range(new_image_size):
@@ -570,8 +567,8 @@ def bilinear_upscale(image: Image, new_image_size: int) -> Image:
 
 # BELOW FUNCTION WAS IMPLEMENTED BY CHATGPT
 def bicubic_upscale(image: Image, new_image_size: int) -> Image:
-        new_image = Image(new_image_size)
         scale_factor = new_image_size / image.size
+        new_image = Image(Boundary([[int(point[0] * scale_factor), int(point[1] * scale_factor)] for point in image.boundary.points]))
 
         def cubic(x):
                 absolute_x = abs(x)
@@ -591,9 +588,7 @@ def bicubic_upscale(image: Image, new_image_size: int) -> Image:
                 result = 0
                 for m in range(-1, 3):
                         for n in range(-1, 3):
-                                x_index = int(constrain(x_floor + m, 0, image.size - 1)[0])
-                                y_index = int(constrain(y_floor + n, 0, image.size - 1)[0])
-                                weight = image[x_index, y_index].weight
+                                weight = image[x_floor + m, y_floor + n].weight
                                 result += weight * cubic(m - x_difference) * cubic(n - y_difference)
 
                 return max(0, result)
@@ -609,7 +604,7 @@ def bicubic_upscale(image: Image, new_image_size: int) -> Image:
 
 
 def gaussian_blur(image: Image, standard_deviation: int | float) -> Image:
-        new_image = Image(image.size)
+        new_image = Image(image.boundary)
 
         def create_kernel(size, sigma):
                 gaussian_filter = [[0] * size for _ in range(size)]
@@ -735,8 +730,8 @@ def gaussian_blur(image: Image, standard_deviation: int | float) -> Image:
 def create_dla_noise(seed: int, initial_size: int, end_size: int, initial_density_threshold: float, density_falloff_extremity: float, density_falloff_bias: float, use_concurrent_walkers: bool, upscale_factor: float, jitter_range: int, smoothness: int) -> List[Image]:
         images = []
 
-        image = Image(initial_size)
-        image_sum = Image(initial_size)
+        image = Image(Boundary([[0, 0], [0, initial_size], [initial_size, 0], [initial_size, initial_size]]))
+        image_sum = Image(Boundary([[0, 0], [0, initial_size], [initial_size, 0], [initial_size, initial_size]]))
 
         random.seed(seed)
 
--- scratch.py
+++ scratch.py
@@ -7,227 +7,218 @@ import numpy as np
 
 
 class Boundary:
-    points: list[list[int]]
-    edges: list[list[list[int]]]
-    size: int
+        points: list[list[int]]
+        edges: list[list[list[int]]]
+        size: int
 
-    def __init__(self, points: list[list[int]]):
-        if len(points) < 3:
-            raise ValueError("Boundary must have at least 3 points")
+        def __init__(self, points: list[list[int]]):
+                if len(points) < 3:
+                        raise ValueError("Boundary must have at least 3 points")
 
-        self.points = points
+                self.points = points
 
-    @property
-    def edges(self) -> list[list[tuple[int, int]]]:
-        edges = []
+        @property
+        def edges(self) -> list[list[tuple[int, int]]]:
+                edges = []
 
-        for i in range(len(self.points)):
-            p0 = self.points[i]
-            p1 = self.points[i + 1] if i < len(self.points) - 1 else self.points[0]
-            line = self.get_line(p0[0], p0[1], p1[0], p1[1])
-            edges.append(line)
+                for i in range(len(self.points)):
+                        p0 = self.points[i]
+                        p1 = self.points[i + 1] if i < len(self.points) - 1 else self.points[0]
+                        line = self.get_line(p0[0], p0[1], p1[0], p1[1])
+                        edges.append(line)
 
-        return edges
+                return edges
 
-    @property
-    def size(self) -> int:
-        x_min = min(p[0] for p in self.points)
-        x_max = max(p[0] for p in self.points)
-        y_min = min(p[1] for p in self.points)
-        y_max = max(p[1] for p in self.points)
+        @property
+        def size(self) -> int:
+                x_min = min(p[0] for p in self.points)
+                x_max = max(p[0] for p in self.points)
+                y_min = min(p[1] for p in self.points)
+                y_max = max(p[1] for p in self.points)
 
-        width = x_max - x_min
-        height = y_max - y_min
+                width = x_max - x_min
+                height = y_max - y_min
 
-        return max(width, height) + 1
+                return max(width, height) + 1
 
-    @staticmethod
-    def get_line(x0: int, y0: int, x1: int, y1: int) -> list[tuple[int, int]]:
-        dx = abs(x1 - x0)
-        dy = abs(y1 - y0)
-        sx = 1 if x0 < x1 else -1
-        sy = 1 if y0 < y1 else -1
-        err = dx - dy
-        line = []
+        @staticmethod
+        def get_line(x0: int, y0: int, x1: int, y1: int) -> list[tuple[int, int]]:
+                dx = abs(x1 - x0)
+                dy = abs(y1 - y0)
+                sx = 1 if x0 < x1 else -1
+                sy = 1 if y0 < y1 else -1
+                err = dx - dy
+                line = []
 
-        while True:
-            line.append((x0, y0))
+                while True:
+                        line.append((x0, y0))
 
-            if x0 == x1 and y0 == y1:
-                break
-            err2 = err * 2
-            if err2 > -dy:
-                err -= dy
-                x0 += sx
-            if err2 < dx:
-                err += dx
-                y0 += sy
+                        if x0 == x1 and y0 == y1:
+                                break
+                        err2 = err * 2
+                        if err2 > -dy:
+                                err -= dy
+                                x0 += sx
+                        if err2 < dx:
+                                err += dx
+                                y0 += sy
 
-        return line
+                return line
 
 
 class Pixel:
-    x: int
-    y: int
-    boundary: bool
-    flooded: bool
-    frozen: bool
-    weight: float
-    struck: Optional[Self]
+        x: int
+        y: int
+        boundary: bool
+        flooded: bool
+        frozen: bool
+        weight: float
+        struck: Optional[Self]
 
-    def __init__(self, x: int, y: int):
-        self.x = x
-        self.y = y
-        self.boundary = False
-        self.flooded = False
-        self.frozen = False
-        self.weight = 0
-        self.struck = None
+        def __init__(self, x: int, y: int):
+                self.x = x
+                self.y = y
+                self.boundary = False
+                self.flooded = False
+                self.frozen = False
+                self.weight = 0
+                self.struck = None
 
 
 class Image:
-    size: int
-    grid: list[list[Pixel]]
-    boundary: Boundary
-    origin: Optional[Pixel]
+        size: int
+        grid: list[list[Pixel]]
+        boundary: Boundary
+        origin: Optional[Pixel]
 
-    def __init__(self, boundary: Boundary):
-        self.boundary = boundary
-        self.size = boundary.size
-        self.grid = [[Pixel(j, i) for j in range(self.size)] for i in range(self.size)]
-        self.origin = None
+        def __init__(self, boundary: Boundary):
+                self.boundary = boundary
+                self.size = boundary.size
+                self.grid = [[Pixel(j, i) for j in range(self.size)] for i in range(self.size)]
+                self.origin = None
 
-        for edge in boundary.edges:
-            for point in edge:
-                self[point[0], point[1], False].boundary = True
+                for edge in boundary.edges:
+                        for point in edge:
+                                self[point[0], point[1], False].boundary = True
 
-        self.__flood_fill()
+                self.__flood_fill()
 
-    def __flood_fill(self):
-        queue = deque()
+        def __flood_fill(self):
+                queue = deque()
 
-        # Initialize the queue with the boundary pixels
-        for i in range(self.size):
-            if not self[0, i, False].boundary:
-                queue.append((0, i))
-            if not self[self.size - 1, i, False].boundary:
-                queue.append((self.size - 1, i))
-            if not self[i, 0, False].boundary:
-                queue.append((i, 0))
-            if not self[i, self.size - 1, False].boundary:
-                queue.append((i, self.size - 1))
+                # Initialize the queue with the boundary pixels
+                for i in range(self.size):
+                        if not self[0, i, False].boundary:
+                                queue.append((0, i))
+                        if not self[self.size - 1, i, False].boundary:
+                                queue.append((self.size - 1, i))
+                        if not self[i, 0, False].boundary:
+                                queue.append((i, 0))
+                        if not self[i, self.size - 1, False].boundary:
+                                queue.append((i, self.size - 1))
 
-        # Perform the flood fill
-        while queue:
-            x, y = queue.popleft()
-            if not self[x, y, False].flooded and not self[x, y, False].boundary:
-                self[x, y, False].flooded = True
-                if x > 0:
-                    queue.append((x - 1, y))
-                if x < self.size - 1:
-                    queue.append((x + 1, y))
-                if y > 0:
-                    queue.append((x, y - 1))
-                if y < self.size - 1:
-                    queue.append((x, y + 1))
+                # Perform the flood fill
+                while queue:
+                        x, y = queue.popleft()
+                        if not self[x, y, False].flooded and not self[x, y, False].boundary:
+                                self[x, y, False].flooded = True
+                                if x > 0:
+                                        queue.append((x - 1, y))
+                                if x < self.size - 1:
+                                        queue.append((x + 1, y))
+                                if y > 0:
+                                        queue.append((x, y - 1))
+                                if y < self.size - 1:
+                                        queue.append((x, y + 1))
 
-    def weights(self) -> list[list[int]]:
-        weights = [[]] * self.size
-        for i in range(self.size):
-            weights[i] = [pixel.weight for pixel in self.grid[i]]
-        return weights
+        def weights(self) -> list[list[int]]:
+                weights = [[]] * self.size
+                for i in range(self.size):
+                        weights[i] = [pixel.weight for pixel in self.grid[i]]
+                return weights
 
-    def graph(self):
-        pass
+        def graph(self):
+                pass
 
-    def traversable(self) -> bool:
-        pass
+        def traversable(self) -> bool:
+                pass
 
-    def show(self) -> None:
-        weights = []
-        for i in range(self.size):
-            weights.append([])
-            for j in range(self.size):
-                if self[j, i, False].flooded:
-                    weights[i].append(-50)
-                elif self[j, i, False].boundary:
-                    weights[i].append(-10)
-                else:
-                    weights[i].append(self[j, i, False].weight)
-        weights = np.array(weights)
+        def show(self) -> None:
+                weights = []
+                for i in range(self.size):
+                        weights.append([])
+                        for j in range(self.size):
+                                if self[j, i, False].flooded:
+                                        weights[i].append(-50)
+                                elif self[j, i, False].boundary:
+                                        weights[i].append(-10)
+                                else:
+                                        weights[i].append(self[j, i, False].weight)
+                weights = np.array(weights)
 
-        plt.imshow(weights, cmap="terrain", origin="upper")
-        plt.colorbar(label="Weight")
-        plt.title("Heightmap Based on 2D Array of Weights")
-        plt.xlabel("X coordinate")
-        plt.ylabel("Y coordinate")
-        plt.show()
+                plt.imshow(weights, cmap="terrain", origin="upper")
+                plt.colorbar(label="Weight")
+                plt.title("Heightmap Based on 2D Array of Weights")
+                plt.xlabel("X coordinate")
+                plt.ylabel("Y coordinate")
+                plt.show()
 
-    def __getitem__(self, index: tuple[int, int, bool]):
-        x: int = index[0]
-        y: int = index[1]
-        clamp: bool = index[2]
+        def __getitem__(self, index: tuple[int, int, bool]):
+                x: int = index[0]
+                y: int = index[1]
+                clamp: bool = index[2]
 
-        if not clamp:
-            return self.grid[y][x]
+                if not clamp:
+                        return self.grid[y][x]
 
-        x = min(max(x, 0), self.size - 1)
-        y = min(max(y, 0), self.size - 1)
+                x = min(max(x, 0), self.size - 1)
+                y = min(max(y, 0), self.size - 1)
 
-        if not self.grid[y][x].flooded:
-            return self.grid[y][x]
+                if not self.grid[y][x].flooded:
+                        return self.grid[y][x]
 
-        # BFS to find the closest boundary pixel
-        queue = deque([(x, y)])
-        visited = {x, y}
+                # BFS to find the closest boundary pixel
+                queue = deque([(x, y)])
+                visited = {x, y}
 
-        while queue:
-            cx, cy = queue.popleft()
-            if self.grid[cy][cx].boundary:
-                return self.grid[cy][cx]
+                while queue:
+                        cx, cy = queue.popleft()
+                        if self.grid[cy][cx].boundary:
+                                return self.grid[cy][cx]
 
-            for nx, ny in [
-                (cx - 1, cy),
-                (cx + 1, cy),
-                (cx, cy - 1),
-                (cx, cy + 1),
-                (cx - 1, cy - 1),
-                (cx + 1, cy + 1),
-                (cx - 1, cy + 1),
-                (cx + 1, cy - 1),
-            ]:
-                if 0 <= nx < self.size and 0 <= ny < self.size and (nx, ny) not in visited:
-                    visited.add((nx, ny))
-                    queue.append((nx, ny))
+                        for nx, ny in [(cx - 1, cy), (cx + 1, cy), (cx, cy - 1), (cx, cy + 1), (cx - 1, cy - 1), (cx + 1, cy + 1), (cx - 1, cy + 1), (cx + 1, cy - 1)]:
+                                if 0 <= nx < self.size and 0 <= ny < self.size and (nx, ny) not in visited:
+                                        visited.add((nx, ny))
+                                        queue.append((nx, ny))
 
-    def __add__(self, other: Self) -> Self:
-        if self.size != other.size:
-            raise ValueError("Image must be the same size")
+        def __add__(self, other: Self) -> Self:
+                if self.size != other.size:
+                        raise ValueError("Image must be the same size")
 
-        if self.boundary.points != other.boundary.points:
-            raise ValueError("Image must have the same boundary")
+                if self.boundary.points != other.boundary.points:
+                        raise ValueError("Image must have the same boundary")
 
-        new_image = Image(Boundary(self.boundary.points))
-        for i in range(self.size):
-            for j in range(self.size):
-                if not self.grid[i][j].flooded and other.grid[i][j].flooded:
-                    new_image.grid[i][j].weight = self.grid[i][j].weight + other.grid[i][j].weight
+                new_image = Image(Boundary(self.boundary.points))
+                for i in range(self.size):
+                        for j in range(self.size):
+                                if not self.grid[i][j].flooded and other.grid[i][j].flooded:
+                                        new_image.grid[i][j].weight = self.grid[i][j].weight + other.grid[i][j].weight
 
-        return new_image
+                return new_image
 
 
 def circle(radius: int, precision=5) -> list[list[int]]:
-    points = []
-    for i in range(precision):
-        theta = 2 * math.pi * i / precision
-        x = round(radius + radius * math.cos(theta))
-        y = round(radius + radius * math.sin(theta))
-        points.append([x, y])
-    return points
+        points = []
+        for i in range(precision):
+                theta = 2 * math.pi * i / precision
+                x = round(radius + radius * math.cos(theta))
+                y = round(radius + radius * math.sin(theta))
+                points.append([x, y])
+        return points
 
 
 if __name__ == "__main__":
-    i1 = Image(Boundary([[0, 0], [10, 0], [5, 10]]))
-    i2 = Image(Boundary([[0, 0], [10, 0], [5, 10]]))
-    i3 = i1 + i2
-    i3.show()
+        i1 = Image(Boundary([[0, 0], [10, 0], [5, 10]]))
+        i2 = Image(Boundary([[0, 0], [10, 0], [5, 10]]))
+        i3 = i1 + i2
+        i3.show()
